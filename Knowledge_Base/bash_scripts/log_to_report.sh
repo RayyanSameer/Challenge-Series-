7a57e2a8eee064ec8e688cba433777c5a9b7c844
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-03-01 13:34:40 +0530

    fixed variable errors

--- Knowledge_Base/bash_scripts/log_to_report.sh
+++ Knowledge_Base/bash_scripts/log_to_report.sh
@@ -15,16 +15,20 @@ fi
 
 LOG_FILE="$1"
 
-if [[! -r "$LOG_FILE" ]]; then
+if [[ ! -r "$LOG_FILE" ]]; then
     echo "Error: Cannot read $LOG_FILE"
     exit 1
 fi
 
 echo "--- Log Report for $LOG_FILE ---"]]
 
-ERRORS=$(grep -c "\[ERROR\]" "$ERROR")
+ERRORS=$(grep -c "\[ERROR\]" "$LOG_FILE")
 WARNS=$(grep -c "\[WARNING\]" "$LOG_FILE")
 INFOS=$(grep -c "\[INFO\]" "$LOG_FILE")
+
+if [ $ERRORS -gt 10 ];then
+    echo -e "\e[31mCRITICAL: High error volume detected!\e[0m"
+fi    
 
 echo "Total Errors:   $ERRORS"
 echo "Total Warnings: $WARNS"
