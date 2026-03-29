a0603a43138ba8e38ff4b7628960d3ae215af69b
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-03-29 12:57:52 +0530

    sysinfo V1 CLI Utility

---
+++ Knowledge_Base/bash_scripts/sysinfo.sh
@@ -0,0 +1,56 @@
+# Prints out system info 
+
+#OS Version
+#Username 
+#Date 
+#Disk
+#CPU and GPU  info 
+#Network Stats 
+#RAM Stats 
+
+#!/bin/bash
+
+DATE=$(date)
+DISK=$(df -h /)
+CPU=$(top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | gawk '{print $2+$4+$6"%"}'
+)
+GPU=$(intel_gpu_top -bn 2 -d 0.01 | grep '^%Cpu' | tail -n 1 | gawk '{print $2+$4+$6"%"}')
+NET=$(nstat)
+MEM=$(free -h)
+echo "Instance Details "  
+hostnamectl
+echo "================================"
+
+echo "Date: $DATE"
+echo  "$(whoami)"
+
+echo "================================"
+
+echo "Disk Usage Stats: "
+echo "$DISK"
+
+echo "================================="
+echo "Memory Usage Stats: "
+echo "Memory: $MEM"
+
+echo "================================"
+echo "CPU Stats: "
+echo "$CPU Usage "
+
+echo "================================"
+echo "GPU Stats: "
+echo "$GPU Usage"
+
+echo "================================"
+echo "Network Stats: "
+echo "$NET"
+
+
+chmod +x sysinfo.sh
+
+
+#Basic Version 
+
+#TODO: Implement a menu system allowing uses to get an overview or a specific stat
+#TODO: Use Interactive Commands to show libraries 
+#TODO: Add CLI Dashboard
