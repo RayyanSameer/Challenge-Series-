51f3c453a7a852279383d455f0f41fbf705405ca
Rayyan Sameer <turbocharged.tomato@gmail.com>
2026-04-05 17:21:01 +0530

    Redis client setup, index route, metrics route, history route, and static file serving.
    Rebuilding the simple system audit tool with Flask and Redis to capture and display system metrics.

---
+++ Knowledge_Base/bash_scripts/sysinfo2.sh
@@ -0,0 +1,18 @@
+#WHAT am i doing ?
+
+#Checking for system info 
+
+#WHAT specifically fo i need ?
+#Basic Usage 
+#Hostname 
+#Disk
+#RAM Stats 
+#Time 
+
+
+#Where is it
+# Hostname 
+# df -g
+# 
+
+
--- Mini_Apps/Simple_System_Audit_Tool_Rebuild/app.py
+++ Mini_Apps/Simple_System_Audit_Tool_Rebuild/app.py
@@ -13,4 +13,72 @@ import os
 import psutil
 import datetime
 from flask import Flask, jsonify, render_template, send_from_directory
-import jsonify from
+
+#The app container and the Redis client setup
+
+app = Flask(__name__, template_folder='../frontend', static_folder='../frontend'
+)
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+
+
+
+#index route to serve the frontend
+app.route('/')
+def index():
+    return render_template('index.html')
+#metrics route to capture and store system metrics in Redis
+app.route('/metrics')
+def metrics():
+    data = {
+        'cpu': psutil.cpu_percent(interval=1),
+        'memory':psutil.virtual_memory().percent,
+        'disk': psutil.disk_usage('/').percent,
+        'timestamp': datetime.datetime.now().isoformat()
+    }
+
+    #Push and trim for 10 entries in Redis
+
+    r.lpush('metrics_history', json.dumps(data))
+    r.ltrim('metrics_history', 0, 9)    
+    return jsonify(data)
+
+#history route to return the last 10 snapshots from Redis
+
+app.route('/history')
+def history():
+    #Get last 10 entries from Redis
+
+    raw_data = r.lrange('metrics_history', 0, -1)
+    history_list = [json.loads(x) for x in raw_data]
+    return jsonify(history_list)
+
+app.route('/<path:path>')
+#Serve static files from the frontend folder
+def send_report(path):
+    return send_from_directory('../frontend', path)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+
+
+#Parts 
+
+#Import 
+#App boilerplate 
+#Redis client setup
+#Index route to serve frontend just called route 
+#Metrics route to capture and store system metrics in Redis
+
+#Use psutil to get CPU, memory, and disk usage, along with a timestamp. Store this data in Redis using lpush and trim to keep only the last 10 entries.
+
+
+#History route to return the last 10 snapshots from Redis
+#Serve static files from the frontend folder
+
+#From the raw data retrieved from Redis, we parse the JSON strings into Python history list and return them as a JSON response.
+
+#if name == main to run the app on host
+
+
