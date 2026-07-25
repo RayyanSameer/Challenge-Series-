51f636eff784e3cf10be6604b0d24d79c811bb8e
RayyanSameer <turbocharged.tomato@gmail.com>
2026-07-26 00:13:16 +0530

    BASH:Frequency_for_parsing

---
+++ Knowledge_Base/bash_scripts/Practice_Scripts/Word_Freq_COunter.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [[ $# -ge 2 ]]; then
+    input_file="$1"
+    words_to_be_excluded_file="$2"
+else
+    read -p "Enter input file path: " input_file
+    read -p "Enter excluded words file path: " words_to_be_excluded_file
+fi
+
+if [[ ! -f "$input_file" ]]; then
+    echo "Error: Input file '$input_file' not found." >&2
+    exit 1
+fi
+
+if [[ ! -f "$words_to_be_excluded_file" ]]; then
+    echo "Error: Excluded words file '$words_to_be_excluded_file' not found." >&2
+    exit 1
+fi
+
+
+
+
+tr '[:upper:]' '[:lower:]' < "$input_file" |
+tr -d '.,!?;:"' |
+tr ' ' '\n' |
+grep -v -w -f  "$words_to_be_excluded_file" |
+sort |
+uniq -c |
+sort -rn |
+head -20
